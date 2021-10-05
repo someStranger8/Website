417ae4f36633b4896539a053f32c6a6b06a29149
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-10-05 11:11:39 -0400

    Create shark_attack.py

---
+++ docs/hack/shark_attack.py
@@ -0,0 +1,51 @@
+
+# imports
+import os
+import cobra as c
+
+# banner
+print("\____)\______                    ______/(____/")
+print("/--^____ __'<    SHARK ATTACK    >'__ ____^--\\")
+print("       )/                            \(")
+print("")
+
+# while loop
+i=1
+while i < 10:
+     shell=input("shark@attack:~$ ")
+     if shell == "clear":
+          os.system("clear")
+     elif shell == "sqlinject":
+          c.sqlinject()
+     elif shell == "exploit":
+          c.exploit()
+     elif shell == "hackwifi":
+          c.hackwifi()
+     elif shell == "hackpasswd":
+          usr=input("[*] enter username: ")
+          wl=input("[*] enter wordlist: ")
+          ip=input("[*] enter ip address: ")
+          c.hackpasswd(usr, wl, ip)
+     elif shell == "mitm":
+          c.mitm()
+     elif shell == "scan":
+          nmap=input("[*] enter ip: ")
+          c.scan(nmap)
+     elif shell == "hackweb":
+          c.hackweb()
+     elif shell == "hackphone":
+          c.hackphone()
+     elif shell == "hackdrone":
+          c.hackdrone()
+     elif shell == "install":
+          pkg=input("[*] enter the name of the package you want to install: ")
+          c.install(pkg)
+     elif shell == "payload":
+          name=input("[*] enter file name: ")
+          c.payload(name)
+     elif shell == "ddos":
+          attack=input("[*] enter ip address: ")
+          c.ddos(attack)
+     else:
+          os.system(shell)
+i+=1
