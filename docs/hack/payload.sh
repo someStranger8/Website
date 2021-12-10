ceb67ecdf327eccfff6786538e4709104cfb7162
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-12-10 12:19:30 -0500

    Delete payload.sh

--- docs/hack/payload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Hacking the world
-
-# Install
-pip3 install sqlmap
-
-# run exploit
-sqlmap -u "http://example.com/" --crawl=5 --random-agent --batch --forms --threads=5 --level=5 --risk=3
